0c19023332e0800db58c3f114911df7c4922896b
Sandro Zappulla <102858707+SandroZappulla@users.noreply.github.com>
2022-12-14 11:40:44 +0100

    Update BrightnessToggler.py

--- BrightnessToggler.py
+++ BrightnessToggler.py
@@ -7,6 +7,7 @@
 # python -m pip install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org screen-brightness-control
 # IMPORT
 import screen_brightness_control as sbc
+import os
 
 # list of all monitor names
 all_monitors = sbc.list_monitors()
@@ -28,12 +29,18 @@ while counter < count_monitors:
 
 # Condition: then all monitors got the brightness level of 100%
 # All monitors toggle the brightness to the toggle_value
+# maximum_value set the Monitor brightness back to its normal brightness level
 toggle_value = 50
+maximum_value = 100
 
 if all_brightness == value:
     sbc.set_brightness(toggle_value)
+    os.system("cls")
+    print("Helligkeit aller Monitore auf", toggle_value,"%" ,"gesetzt!")
 else:
-    sbc.set_brightness(100)
+    sbc.set_brightness(maximum_value)
+    os.system("cls")
+    print("Helligkeit aller Monitore auf", maximum_value,"%" ,"gesetzt!")
 
 
 # if you like to use any specific monitor for brightness level then use this upcoming lines
